9eb241d6c3449cd35cfbfcdffe5a68931a943ebc
Frank Odom <frank.odom.iii@gmail.com>
2022-01-30 18:24:04 -0600

    Build out the Streamlit app

--- app.py
+++ app.py
@@ -1,3 +1,56 @@
 import streamlit as st
+from streamlit import legacy_caching
 
-st.
+from wordle.game import Wordle
+
+
+MARKDOWN_ANSWER_TEMPLATE = """
+The correct answer was: <p style='color:Green;font-size:24px;text-align:center'><b>{answer}</b></p>
+"""
+WORDLE_RULES = """
+* Guess the WORDLE in 6 tries.
+
+* Each guess must be a valid 5 letter word.
+
+* The colors of the letters show how close your guess was to the word.
+    > <span style='color:Red'>RED</span> letters are not in the word.
+    >
+    > <span style='color:Yellow'>YELLOW</span> letters are in the word but in the wrong spot.
+    >
+    > <span style='color:Green'>GREEN</span> letters are in the word and in the correct spot.
+"""
+
+
+@st.cache(allow_output_mutation=True)
+def new_game():
+    return Wordle()
+
+
+st.title("Wordle!")
+
+with st.expander("Game Rules"):
+    st.markdown(WORDLE_RULES, unsafe_allow_html=True)
+
+game = new_game()
+game.render_streamlit()
+
+if not game._done:
+    with st.form("Form"):
+        guess = st.text_input("Enter your guess: ").strip()
+        if st.form_submit_button("Submit"):
+            game.step(guess)
+            st.experimental_rerun()
+
+else:
+    if game._success:
+        st.subheader("You won! :)")
+    else:
+        st.subheader("You lost. :(")
+        st.markdown(
+            MARKDOWN_ANSWER_TEMPLATE.format(answer=game._word.upper()),
+            unsafe_allow_html=True,
+        )
+
+    if st.button("New Game"):
+        legacy_caching.clear_cache()
+        st.experimental_rerun()
--- wordle/game.py
+++ wordle/game.py
@@ -7,16 +7,21 @@ from wordle.data import load_word_frequencies
 
 WORD_LENGTH = 5
 STEPS_PER_GAME = 6
+MARKDOWN_LETTER_TEMPLATE = """
+<p style='color:{color};background-color:gray;font-size:24px;text-align:center'><b>{letter}</b></p>
+"""
+EMPTY_LETTER = {"letter": "_", "inWord": False, "inCorrectPosition": False}
 
 
 class Wordle:
-    def __init__(self, word_bank_size: int = 2500, silent: bool = False):
+    def __init__(self, word_bank_size: int = 1500, silent: bool = False):
         word_bank = load_word_frequencies(max_words=word_bank_size)
-        self.__word = random.choice(list(word_bank.keys()))
+        self._word = random.choice(list(word_bank.keys()))
         self.silent = silent
 
-        self._step: int = 1
-        self._done: bool = False
+        self._step = 1
+        self._done = False
+        self._success = False
         self.history: List[Dict] = []
 
     @staticmethod
@@ -31,8 +36,7 @@ class Wordle:
             ],
         }
 
-    @staticmethod
-    def _print_step_info(info: Dict):
+    def _print_step_info(self, info: Dict):
         for letter in info["letters"]:
             if letter["inCorrectPosition"]:
                 color = Fore.GREEN
@@ -42,19 +46,22 @@ class Wordle:
                 color = Fore.RED
             print(f"{color}{letter['letter']}{Fore.RESET}", end=" ")
 
-        print()
+        print("\n")
         if info["success"]:
             print(f"{Fore.GREEN}YOU WIN!{Fore.RESET}")
-        else:
-            print()
+        elif info["done"]:
+            print(f"{Fore.RED}You lost :(")
+            print(f"The word was: {Fore.GREEN}{self._word}{Fore.RESET}")
+        print()
 
     def step(self, guess: str) -> Dict:
-        info = self._evaluate_guess(guess=guess, truth=self.__word)
+        info = self._evaluate_guess(guess=guess, truth=self._word)
         info["step"] = self._step
         info["done"] = info["success"] or self._step == STEPS_PER_GAME
 
         self._step += 1
         self._done = info["done"]
+        self._success = info["success"]
         self.history.append(info)
 
         if not self.silent:
@@ -68,8 +75,41 @@ class Wordle:
         while not self._done:
             print(f"Step {self._step} of {STEPS_PER_GAME}")
             guess = input("Enter a guess: ").strip("\n\r")
-            info = self.step(guess)
-            self._print_step_info(info)
+            _ = self.step(guess)
+
+    def _render_step_info_streamlit(self, info: Dict):
+        import streamlit as st
+
+        columns = st.columns(5)
+        letters = info.get("letters", [EMPTY_LETTER] * 5)
+
+        for column, letter in zip(columns, letters):
+            if letter["letter"] == "_":
+                color = "Gray"
+            elif letter["inCorrectPosition"]:
+                color = "Green"
+            elif letter["inWord"]:
+                color = "Yellow"
+            else:
+                color = "Red"
+            with column:
+                st.markdown(
+                    MARKDOWN_LETTER_TEMPLATE.format(
+                        color=color, letter=letter["letter"].upper()
+                    ),
+                    unsafe_allow_html=True,
+                )
+
+    def _render_empty_step_streamlit(self):
+        self._render_step_info_streamlit()
+
+    def render_streamlit(self):
+        for step in self.history:
+            self._render_step_info_streamlit(step)
+
+        remaining_steps = STEPS_PER_GAME - len(self.history)
+        for _ in range(remaining_steps):
+            self._render_step_info_streamlit(info={})
 
 
 if __name__ == "__main__":
