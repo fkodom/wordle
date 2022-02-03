8d874c111fa0cd3a86c191a32310c78f62e1c987
Frank Odom <frank.odom.iii@gmail.com>
2022-02-03 15:55:09 -0600

    Also store words in Drive, automatically download if not found locally.

--- wordle/data.py
+++ wordle/data.py
@@ -2,12 +2,26 @@ import os
 from functools import lru_cache
 from typing import List
 
+import gdown
+
+WORDS_URL = (
+    "https://drive.google.com/uc?id=1upgBKczLa9CU1q1V-_Hsi3ImPfPGqevb"
+    # https://drive.google.com/file/d/1upgBKczLa9CU1q1V-_Hsi3ImPfPGqevb/view?usp=sharing
+)
 WORDS_PATH = os.path.join(
     os.path.dirname(__file__), os.path.pardir, "data", "words.txt"
 )
 
 
+def _download_words():
+    os.makedirs(os.path.dirname(WORDS_PATH), exist_ok=True)
+    gdown.download(WORDS_URL, WORDS_PATH)
+
+
 @lru_cache()
 def load_words() -> List[str]:
+    if not os.path.exists(WORDS_PATH):
+        _download_words()
+
     with open(WORDS_PATH, "r") as f:
         return [line.lower().strip() for line in f.readlines()]
